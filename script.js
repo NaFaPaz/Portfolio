deb6304b75a597f17553ba49f41b8299f43974b6
NaFaPaz <nfanego@gmail.com>
2021-12-16 19:50:34 -0300

    add skills section

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const navBar = document.querySelector('.nav-bar');
-const menuBtn = document.querySelector('.menu-btn');
+const menuBtn = document.querySelectorAll('.toggle');
 
-const handleMenu = () => {
+
+const handleMenu = (event) => {
     if (navBar.style.display === 'flex'){
         navBar.style.display = 'none';
     } else {
         navBar.style.display = 'flex';
     }
+    
 }
 
-console.log(navBar.style.display)
-menuBtn.addEventListener('click', handleMenu);
+
+
+window.addEventListener('resize', ()=>{
+    var w = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
+    var targetWidth = 780;
+
+    if ( w <= targetWidth) {     
+        for (element of menuBtn) {
+        navBar.style.display = 'none';
+        element.addEventListener('click', handleMenu)
+        };
+    } else if(w > targetWidth) {
+        navBar.style.display = 'flex';
+        for (element of menuBtn) {
+        element.removeEventListener('click', handleMenu)
+        };
+    }
+})
+
+
